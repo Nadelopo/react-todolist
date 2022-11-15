cba4266f5a836c205605a291ef79876a9b4aa717
Nadelopo <mistrmorsikk@mail.ru>
2022-11-16 01:53:13 +0400

    update accordion

--- src/components/UI/Accordion/index.tsx
+++ src/components/UI/Accordion/index.tsx
@@ -5,7 +5,7 @@ import React, {
   ReactElement,
   RefObject,
   useEffect,
-  useMemo,
+  useState,
 } from 'react'
 import S from './Accordion.module.sass'
 
@@ -16,20 +16,32 @@ interface Iaccordion {
   paddingBottom?: number
 }
 
+interface Istyles {
+  paddingTop: string
+  paddingBottom: string
+  height: string
+}
+
 export const Accordion: React.FC<Iaccordion> = (props) => {
   const refsArray: RefObject<HTMLElement>[] = props.children.map(() =>
     createRef()
   )
-
-  const [heightFull, setHeightFull] = React.useState(0)
+  const [heightFull, setHeightFull] = React.useState<number[]>([])
+  const [styles, setStyles] = useState<(Istyles | null)[]>([])
 
   useEffect(() => {
+    setHeightFull([])
     refsArray.forEach((el) => {
-      if (el.current) setHeightFull(el.current.scrollHeight)
+      if (el.current) {
+        setHeightFull((v) => {
+          if (el.current) return [...v, el.current.scrollHeight]
+          else return v
+        })
+      }
     })
   }, [])
 
-  const calcHeigth = useMemo(() => {
+  const calcHeigth = (heightF: number) => {
     let paddingTop = '0px'
     let paddingBottom = '0px'
     let height = '0px'
@@ -37,21 +49,26 @@ export const Accordion: React.FC<Iaccordion> = (props) => {
     paddingTop = (props.paddingTop ?? 5) + 'px'
     paddingBottom = (props.paddingBottom ?? 5) + 'px'
     height =
-      heightFull + (props.paddingTop ?? 5) + (props.paddingBottom ?? 5) + 'px'
+      heightF + (props.paddingTop ?? 5) + (props.paddingBottom ?? 5) + 'px'
     return props.visible
       ? {
           paddingTop,
           paddingBottom,
           height,
         }
-      : ''
+      : null
+  }
+
+  useEffect(() => {
+    setStyles([])
+    heightFull.forEach((height) => setStyles((v) => [...v, calcHeigth(height)]))
   }, [props.visible, heightFull])
 
   return (
     <div className={`${S.accordion} ${props.visible && S.active}`}>
       {Children.map(props.children, (child, i) =>
         cloneElement(child, {
-          style: { ...calcHeigth },
+          style: { ...styles[i] },
           ref: refsArray[i],
         })
       )}
