21e9dfd0549913797bdeb6c8cba10b6791628c10
Nadelopo <mistrmorsikk@mail.ru>
2022-11-17 22:23:51 +0400

    add private routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Provider } from 'react-redux'
-import { BrowserRouter as Router, useRoutes } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  useNavigate,
+  useRoutes,
+} from 'react-router-dom'
 import AppWrapper from './App'
 import { store } from './redux/store'
 import Auth from '@/pages/Auth'
 import { Home } from './pages/Home'
 import { Profile } from './pages/Profile'
+import { supabase } from './supabase'
 
 export const routesName = {
   Auth: '/auth',
   Home: '/',
   Profile: '/profile',
 }
-
+const locate = window.location.pathname
 const App = () => {
-  const routes = useRoutes([
+  const navigate = useNavigate()
+  const [isLogin, setIsLogin] = useState(false)
+
+  const token = JSON.parse(localStorage.getItem('supabase.auth.token') || '{}')
+    ?.currentSession?.access_token
+  if (token) {
+    supabase.auth.api.getUser(token).then(({ data, error }) => {
+      if (data) setIsLogin(true)
+      if (error) console.log(error)
+    })
+  }
+
+  useEffect(() => {
+    if (isLogin) navigate(locate)
+    else navigate('/auth')
+  }, [isLogin])
+
+  return useRoutes([
     { path: routesName.Auth, element: <Auth /> },
     { path: routesName.Home, element: <Home /> },
     { path: routesName.Profile, element: <Profile /> },
   ])
-  return routes
 }
 
 export const Index = () => {
